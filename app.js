2034d5b1a48c369a9851d5ffa79d44401457b4b3
unknown <saisk73@gmail.com>
2020-09-10 09:41:22 +0530

    Revert "all methods"
    This reverts commit ca955f6197cf99b304584c745014436583e44edf.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const userRouter = require("./api/users/user.router");
 const membershiptypesRouter = require("./api/membershiptypes/membershiptypes.router"); //
 const membersRouter = require("./api/members/members.router"); //
-app.use(require('cors')({ origin: '*',  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",}));
+app.use(require('cors')({ origin: '*' }));
 app.use(express.json());
 
 const referrerPolicy = require('referrer-policy')
