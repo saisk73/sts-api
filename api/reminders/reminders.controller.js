7ec22099a84e0ebd2a29bd4aea85c683121080b4
sathish9666 <sathish9666@users>
2020-10-12 12:11:03 +0530

    update

--- api/reminders/reminders.controller.js
+++ api/reminders/reminders.controller.js
@@ -1,5 +1,9 @@
 const {
-  getMemberById
+  getTodayBirthdayMembers,
+  getMemberReminderdays,
+  getMembershipEndDays,
+  getchildReminderdays,
+  getChildEndDays
 } = require("./reminders.service");
 require("dotenv").config();
 const ejs = require("ejs");
@@ -13,20 +17,149 @@ var nodeMailer = require('nodemailer');
 var current_date =  moment().format('YYYY-MM-DD');
 var rand,rand2,host,link,links,linkse,rand3;
 var cron = require('node-cron');
-cron.schedule('* * * * *', () => {
-  getMemberById(1, (err, results) => {
+
+cron.schedule('0 0 0 * * *', () => {
+// cron.schedule('* * * * * *', () => {
+  getTodayBirthdayMembers(current_date, (err, results) => {
     if (err) {
       console.log(err);
       return;
     }
-    
-    if(!results){
-      console.log('No Data Found');
-    }
+    if(results){
+      results.forEach(element => { 
+         let transporter = nodeMailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'svapps.websts@gmail.com',
+                pass: '2020#2020'
+            }
+        });
+      let emailTemplatems;
+      ejs
+      .renderFile(path.join(__dirname, "views/birthday.ejs"), {
+        user_firstname: element.full_name
+      })
+    .then(result => {
+      emailTemplatems=result;
+        let mailOptions = {
+            from: 'svapps.websts@gmail.com', // sender address
+            to: element.email,// list of receivers
+            subject: 'Birthday Wishes From STS', // Subject line
+            text:'Wish you many more returns of the day.', // plain text body
+           //html : "Hello,"+req.body.full_name+" Thankyou for register with STS<br> Please Click on the link to verify your email.<br><a href="+linkse+">Click here to verify</a>"  // html body
+       html:emailTemplatems
+        };
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+                res.render('index');
+           })});
 
-    results.password = undefined;
-    console.log('Data Found');
+
+        // console.log(element.full_name);
+    })
+    }
   });
+
+  getMemberReminderdays((err, results1) => {
+    if(results1){
+      results1.forEach(element => { 
+        getMembershipEndDays(element.days,(err, results2) => {
+          results2.forEach(element => {
+            let transporter = nodeMailer.createTransport({
+              host: 'smtp.gmail.com',
+              port: 465,
+              secure: true,
+              auth: {
+                  user: 'svapps.websts@gmail.com',
+                  pass: '2020#2020'
+              }
+          });
+        let emailTemplatems;
+        ejs
+        .renderFile(path.join(__dirname, "views/reminder_membership.ejs"), {
+          user_firstname: element.full_name,
+          end_date: element.membership_enddate
+        })
+      .then(result => {
+        emailTemplatems=result;
+          let mailOptions = {
+              from: 'svapps.websts@gmail.com', // sender address
+              to: element.email,// list of receivers
+              subject: 'Membership Renewal Reminder From STS', // Subject line
+              text:'Your Membership expiry date is '+element.membership_enddate, // plain text body
+             //html : "Hello,"+req.body.full_name+" Thankyou for register with STS<br> Please Click on the link to verify your email.<br><a href="+linkse+">Click here to verify</a>"  // html body
+         html:emailTemplatems
+          };
+          transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                  return console.log(error);
+              }
+              console.log('Message %s sent: %s', info.messageId, info.response);
+                  res.render('index');
+             })});
+
+          })
+        })
+      })
+    }
+  })
+
+  getchildReminderdays((err, results3) => {
+    if(results3){
+      results3.forEach(element => { 
+         var ttl_days = 6570 - element.days;
+        getChildEndDays(ttl_days,(err, results4) => {
+          results4.forEach(element => {
+            let transporter = nodeMailer.createTransport({
+              host: 'smtp.gmail.com',
+              port: 465,
+              secure: true,
+              auth: {
+                  user: 'svapps.websts@gmail.com',
+                  pass: '2020#2020'
+              }
+          });
+        let emailTemplatems;
+        ejs
+        .renderFile(path.join(__dirname, "views/reminder_child.ejs"), {
+          user_firstname: element.full_name,
+          child_name: element.child_name
+        })
+      .then(result => {
+        emailTemplatems=result;
+          let mailOptions = {
+              from: 'svapps.websts@gmail.com', // sender address
+              to: element.email,// list of receivers
+              subject: 'Reminder From STS', // Subject line
+              text:'Your child '+element.child_name+' age is going to complete 18 years.', // plain text body
+             //html : "Hello,"+req.body.full_name+" Thankyou for register with STS<br> Please Click on the link to verify your email.<br><a href="+linkse+">Click here to verify</a>"  // html body
+         html:emailTemplatems
+          };
+          transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                  return console.log(error);
+              }
+              console.log('Message %s sent: %s', info.messageId, info.response);
+                  res.render('index');
+             })});
+
+          })
+        })
+      })
+    }
+  })
+
+
+
+
+
+
+
 });
 
 module.exports = {
