3ae0cd49d027f7853caec5da6bfef0e156ced77d
sathish9666 <sathish9666@users>
2020-10-27 16:55:27 +0530

    update

--- api/members/members.service.js
+++ api/members/members.service.js
@@ -1342,7 +1342,7 @@ module.exports = {
         if (error) {
           callBack(error);
         }
-        return callBack(null, results[0]);
+        return callBack(null, results);
       }
     );
   },
