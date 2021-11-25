a635080abf48d0eadc81ec9f79369f8c649c7806
sathish9666 <60251482+sathish9666@users.noreply.github.com>
2021-11-25 10:47:20 +0530

    update

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ app.use(require('cors')({ origin: '*' }));
 // app.use(bodyParser({limit: '5mb'}));
 app.use(express.json({limit: '50mb'}));
 app.use(express.json());
-
-
-
 app.use("/api/users", userRouter);
 app.use("/api/membershiptypes", membershiptypesRouter); //
 app.use("/api/members", membersRouter); //
